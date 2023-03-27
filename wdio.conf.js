46b5d066228ae0e923172eaf6687a5b228c63e93
Anil Ca <anilcaalexander@gmail.com>
2023-03-27 12:44:17 +0530

    Database testing with automation using wdio

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,3 @@
-let apiCalls;
-const logToFile = require("log-to-file");
-const allureReporter = require("@wdio/allure-reporter").default;
 const allure = require("allure-commandline");
 const fs = require("fs");
 const sql = require('mssql');
@@ -136,7 +133,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [["chromedriver"]],
+  services: ["chromedriver"],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
